4345288c9ece5d4e2e30382a0ebbc733c607fcbb
WainainaMark <teve.designs@gmail.com>
2025-03-24 17:26:17 +0300

    remove

--- smartstock/settings.py
+++ smartstock/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
-    'rest_framework',
     'user',
     'home',
     'frontend',
